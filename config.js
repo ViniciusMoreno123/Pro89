2ff7a6d222344767cbb29fd19ebda46c64c00321
ViniciusMoreno123 <93357216+ViniciusMoreno123@users.noreply.github.com>
2022-12-21 15:10:38 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsjdOE-LACNWeiL3vk44_zq2xo_Z8b_Q8",
+    authDomain: "espectagrama-335e7.firebaseapp.com",
+    databaseURL: "https://espectagrama-335e7-default-rtdb.firebaseio.com",
+    projectId: "espectagrama-335e7",
+    storageBucket: "espectagrama-335e7.appspot.com",
+    messagingSenderId: "755610187229",
+    appId: "1:755610187229:web:65e2cf4a482f13531ef234"
+};
